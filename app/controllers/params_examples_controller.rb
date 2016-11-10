60b83cf5af83dafbbea763ddcefe8fabdef2f8f7
Chris Woodside <christopherwoodside@gmail.com>
2016-11-09 21:12:52 -0600

    Updated HTML pages

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,13 +1,34 @@
 class ParamsExamplesController < ApplicationController
   def query_params_method
-    @user_name = params["user_name"].upcase
-    @message = "Your name begins with the first letter of the alphabet!"
+    @message = params["my_message"]
+    @message2 = params["another_message"]
     render 'query_params.html.erb'
   end
 
-  def number_game_method
-    @magic_number = 75
-    @user_number = params["user_number"]
-    render 'number_game.html.erb'
+  def url_segment_params_method
+    @value = params["this_is_a_variable"]
+    render 'url_segment_params.html.erb'
+  end
+
+  def whatever_name_method
+    @name = params["query_name"]
+    @message = ""
+    if @name.upcase.starts_with?("A")
+      @message = "Hey your name starts with A!"
+    end
+    render 'name.html.erb'
+  end
+
+  def guessing_game_method
+    winning_number = 32
+    @guess = params["user_variable"].to_i
+    if @guess > winning_number
+      @message = "Pick lower!"
+    elsif @guess < winning_number
+      @message = "Pick higher!"
+    else
+      @message = "You win!"
+    end
+    render 'guess.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/query_params_url' => 'params_examples#query_params_method'
-  get '/number_game_url' => 'params_examples#number_game_method'
+  get '/url_segment_params_url/:this_is_a_variable' => 'params_examples#url_segment_params_method'
+  get '/name_url' => 'params_examples#whatever_name_method'
+  get '/guessing_game_url/:user_variable' => 'params_examples#guessing_game_method'
 end
